2364cd4d73abd3dadde267e15389d41a79d6c38b
Mateusz Chmielewski <hello@itsmattch.com>
2024-10-14 10:35:14 +0200

    Include workbench dir

---
+++ workbench/bootstrap/cache/packages.php
@@ -0,0 +1,102 @@
+<?php return array (
+  'laravel/pennant' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pennant\\PennantServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Feature' => 'Laravel\\Pennant\\Feature',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'lorisleiva/laravel-actions' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Lorisleiva\\Actions\\ActionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Action' => 'Lorisleiva\\Actions\\Facades\\Actions',
+    ),
+  ),
+  'lorisleiva/lody' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Lorisleiva\\Lody\\LodyServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Lody' => 'Lorisleiva\\Lody\\Lody',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'orchestra/canvas' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orchestra\\Canvas\\LaravelServiceProvider',
+    ),
+  ),
+  'orchestra/canvas-core' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orchestra\\Canvas\\Core\\LaravelServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ray' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelRay\\RayServiceProvider',
+    ),
+  ),
+  'gildsmith/core-api' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Gildsmith\\CoreApi\\Providers\\CoreServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gildsmith' => 'Gildsmith\\CoreApi\\Facades\\Gildsmith',
+    ),
+  ),
+);
