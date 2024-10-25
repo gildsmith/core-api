462c74d87ab1171a8ead584a45c4118254990c0f
Mateusz Chmielewski <hello@itsmattch.com>
2024-10-25 20:38:59 +0200

    Temporarily remove channel routes

--- routes/channels.php
+++ routes/channels.php
@@ -1,24 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use Gildsmith\CoreApi\Actions\Channels\ChannelCreate;
-use Gildsmith\CoreApi\Actions\Channels\ChannelDelete;
-use Gildsmith\CoreApi\Actions\Channels\ChannelIndex;
-use Gildsmith\CoreApi\Actions\Channels\ChannelUpdate;
-use Gildsmith\CoreApi\Actions\Channels\Pivot\AttachCurrency;
-use Gildsmith\CoreApi\Actions\Channels\Pivot\AttachLanguage;
-use Gildsmith\CoreApi\Actions\Channels\Pivot\DetachCurrency;
-use Gildsmith\CoreApi\Actions\Channels\Pivot\DetachLanguage;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', ChannelIndex::class);
-Route::post('/', ChannelCreate::class);
-Route::delete('{channel}', ChannelDelete::class);
-Route::patch('{channel}', ChannelUpdate::class);
-
-Route::post('{channel}/languages', AttachLanguage::class);
-Route::delete('{channel}/languages/{language}', DetachLanguage::class);
-
-Route::post('{channel}/currencies', AttachCurrency::class);
-Route::delete('{channel}/currencies/{currency}', DetachCurrency::class);
